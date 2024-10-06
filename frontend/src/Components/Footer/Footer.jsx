f74a740de90b836cf2a9b4385ab16f9cb702c455
Dayani-Hewawasam <hewawasam_adk_e23@engug.ruh.ac.lk>
2024-10-06 16:04:23 +0530

    some changes

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
-import './Footer.css'
-import { Link } from 'react-router-dom'
-import footer_logo from '../Assets/logo.png'
-import instagram_icon from '../Assets/instagram_icon.png'
-import whatsapp_icon from '../Assets/whatsapp_icon.png'
-import facebook_icon from '../Assets/facebook_icon.png'
-import gmail_icon from '../Assets/gmail_icon.png'
+import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+import footer_logo from '../Assets/logo.png';
+import instagram_icon from '../Assets/instagram_icon.png';
+import whatsapp_icon from '../Assets/whatsapp_icon.png';
+import facebook_icon from '../Assets/facebook_icon.png';
+import gmail_icon from '../Assets/gmail_icon.png';
 
 export const Footer = () => {
   return (
     <div className='footer'>
-        <div className="footer-logo">
-         <img src={footer_logo} alt="" />
-         <p>SANDU FASHION</p>
-        </div>
-       
-    <p><img src={gmail_icon} alt="" className="icon-gap"/>sandufashion40@gmail.com</p>
-    <p><img src={whatsapp_icon} alt="" className="icon-gap"/>0767685774</p>
-    <p><img src={facebook_icon} alt="" className="icon-gap"/>Sandu Fashion</p>
-    <p><img src={instagram_icon} alt="" className="icon-gap"/>sandu_fashion40</p>
-        
-        
-        <div className="footer-copyright">
-            <hr />
-            <p>Copyright @ 2023 - All Right Reserved</p>
-     </div>
+      {/* First Column: Logo */}
+      <div className="footer-logo">
+        <img src={footer_logo} alt="Sandu Fashion Logo" />
+        <p>SANDU FASHION</p>
+      </div>
+
+      {/* Second Column: Footer Links */}
+      <div className="footer-links-container">
+        <p><img src={gmail_icon} alt="Gmail Icon" className="icon-gap" />sandufashion40@gmail.com</p>
+        <p><img src={whatsapp_icon} alt="WhatsApp Icon" className="icon-gap" />0767685774</p>
+        <p><img src={facebook_icon} alt="Facebook Icon" className="icon-gap" />Sandu Fashion</p>
+        <p><img src={instagram_icon} alt="Instagram Icon" className="icon-gap" />sandu_fashion40</p>
+      </div>
+
+      {/* Third Column: Newsletter */}
+      <div className="footer-newsletter">
+        <h3>Subscribe to Our Newsletter</h3>
+        <p>Get the latest news and updates right in your inbox.</p>
+        <input type="email" placeholder="Enter your email" />
+        <button type="submit">Subscribe</button>
+      </div>
+
+      {/* Footer Copyright Section */}
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright @ 2023 - All Rights Reserved</p>
+      </div>
     </div>
-  )
-}
-export default Footer
+  );
+};
+
+export default Footer;
